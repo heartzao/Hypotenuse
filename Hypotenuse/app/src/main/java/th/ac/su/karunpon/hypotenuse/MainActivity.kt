8b09a534cd06114d35227b6a52773f047df06cad
heart <hadza369@gmail.com>
2021-01-31 12:11:03 +0700

    Hypotenuse

--- Hypotenuse/app/src/main/java/th/ac/su/karunpon/hypotenuse/MainActivity.kt
+++ Hypotenuse/app/src/main/java/th/ac/su/karunpon/hypotenuse/MainActivity.kt
@@ -2,10 +2,29 @@ package th.ac.su.karunpon.hypotenuse
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val a = findViewById<EditText>(R.id.a)
+        val b = findViewById<EditText>(R.id.b)
+        val Cal = findViewById<Button>(R.id.Cal)
+        val show = findViewById<TextView>(R.id.show)
+
+        Cal.setOnClickListener() {
+            var a: Double = a.text.toString().toDouble()
+            var b: Double = b.text.toString().toDouble()
+            var da = a * a
+            var db = b * b
+            var squareRoot = Math.sqrt(da + db)
+            show.text = squareRoot.toString()
+        }
+
+
     }
 }
